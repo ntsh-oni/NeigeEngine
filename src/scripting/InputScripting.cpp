9490a8ed6a023059ae03a1ac96b813cafc95a41b
ntsh-oni <teamnutshell.dev@gmail.com>
2021-05-18 23:20:01 +0200

    error checking for scripting

--- src/scripting/InputScripting.cpp
+++ src/scripting/InputScripting.cpp
@@ -11,107 +11,113 @@ int InputScripting::getKeyState(lua_State* L) {
 	int n = lua_gettop(L);
 	if (n == 1) {
 		std::string key = lua_tostring(L, 1);
-		if (key == "q") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.qKey));
-		}
-		else if (key == "w") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.wKey));
-		}
-		else if (key == "e") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.eKey));
-		}
-		else if (key == "r") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.rKey));
-		}
-		else if (key == "t") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.tKey));
-		}
-		else if (key == "y") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.yKey));
-		}
-		else if (key == "u") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.uKey));
-		}
-		else if (key == "i") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.iKey));
-		}
-		else if (key == "o") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.oKey));
-		}
-		else if (key == "p") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.pKey));
-		}
-		else if (key == "a") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.aKey));
-		}
-		else if (key == "s") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.sKey));
-		}
-		else if (key == "d") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.dKey));
-		}
-		else if (key == "f") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.fKey));
-		}
-		else if (key == "g") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.gKey));
-		}
-		else if (key == "h") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.hKey));
-		}
-		else if (key == "j") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.jKey));
-		}
-		else if (key == "k") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.kKey));
-		}
-		else if (key == "l") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.lKey));
-		}
-		else if (key == "z") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.zKey));
-		}
-		else if (key == "x") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.xKey));
-		}
-		else if (key == "c") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.cKey));
-		}
-		else if (key == "v") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.vKey));
-		}
-		else if (key == "b") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.bKey));
-		}
-		else if (key == "n") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.nKey));
-		}
-		else if (key == "m") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.mKey));
-		}
-		else if (key == "left") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.leftKey));
-		}
-		else if (key == "right") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.rightKey));
-		}
-		else if (key == "up") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.upKey));
-		}
-		else if (key == "down") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.downKey));
-		}
-		else if (key == "escape") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.escapeKey));
-		}
-		else if (key == "space") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.spaceKey));
-		}
-		else if (key == "shift") {
-			lua_pushnumber(L, static_cast<int>(keyboardInputs.shiftKey));
-		}
+		if (lua_isstring(L, -1)) {
+			if (key == "q") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.qKey));
+			}
+			else if (key == "w") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.wKey));
+			}
+			else if (key == "e") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.eKey));
+			}
+			else if (key == "r") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.rKey));
+			}
+			else if (key == "t") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.tKey));
+			}
+			else if (key == "y") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.yKey));
+			}
+			else if (key == "u") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.uKey));
+			}
+			else if (key == "i") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.iKey));
+			}
+			else if (key == "o") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.oKey));
+			}
+			else if (key == "p") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.pKey));
+			}
+			else if (key == "a") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.aKey));
+			}
+			else if (key == "s") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.sKey));
+			}
+			else if (key == "d") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.dKey));
+			}
+			else if (key == "f") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.fKey));
+			}
+			else if (key == "g") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.gKey));
+			}
+			else if (key == "h") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.hKey));
+			}
+			else if (key == "j") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.jKey));
+			}
+			else if (key == "k") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.kKey));
+			}
+			else if (key == "l") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.lKey));
+			}
+			else if (key == "z") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.zKey));
+			}
+			else if (key == "x") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.xKey));
+			}
+			else if (key == "c") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.cKey));
+			}
+			else if (key == "v") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.vKey));
+			}
+			else if (key == "b") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.bKey));
+			}
+			else if (key == "n") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.nKey));
+			}
+			else if (key == "m") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.mKey));
+			}
+			else if (key == "left") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.leftKey));
+			}
+			else if (key == "right") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.rightKey));
+			}
+			else if (key == "up") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.upKey));
+			}
+			else if (key == "down") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.downKey));
+			}
+			else if (key == "escape") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.escapeKey));
+			}
+			else if (key == "space") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.spaceKey));
+			}
+			else if (key == "shift") {
+				lua_pushnumber(L, static_cast<int>(keyboardInputs.shiftKey));
+			}
 
-		return 1;
+			return 1;
+		}
+		else {
+			NEIGE_SCRIPT_ERROR("Function \"getKeyState(string key)\" takes 1 string parameter.");
+			return 0;
+		}
 	}
 	else {
 		NEIGE_SCRIPT_ERROR("Function \"getKeyState(string key)\" takes 1 string parameter.");
@@ -123,32 +129,38 @@ int InputScripting::getMouseButtonState(lua_State* L) {
 	int n = lua_gettop(L);
 	if (n == 1) {
 		std::string button = lua_tostring(L, 1);
-		if (button == "left") {
-			lua_pushnumber(L, static_cast<int>(mouseInputs.leftButton));
-		}
-		else if (button == "right") {
-			lua_pushnumber(L, static_cast<int>(mouseInputs.rightButton));
-		}
-		else if (button == "middle") {
-			lua_pushnumber(L, static_cast<int>(mouseInputs.middleButton));
-		}
-		else if (button == "4") {
-			lua_pushnumber(L, static_cast<int>(mouseInputs.fourButton));
-		}
-		else if (button == "5") {
-			lua_pushnumber(L, static_cast<int>(mouseInputs.fiveButton));
-		}
-		else if (button == "6") {
-			lua_pushnumber(L, static_cast<int>(mouseInputs.sixButton));
-		}
-		else if (button == "7") {
-			lua_pushnumber(L, static_cast<int>(mouseInputs.sevenButton));
-		}
-		else if (button == "8") {
-			lua_pushnumber(L, static_cast<int>(mouseInputs.eightButton));
-		}
+		if (lua_tostring(L, -1)) {
+			if (button == "left") {
+				lua_pushnumber(L, static_cast<int>(mouseInputs.leftButton));
+			}
+			else if (button == "right") {
+				lua_pushnumber(L, static_cast<int>(mouseInputs.rightButton));
+			}
+			else if (button == "middle") {
+				lua_pushnumber(L, static_cast<int>(mouseInputs.middleButton));
+			}
+			else if (button == "4") {
+				lua_pushnumber(L, static_cast<int>(mouseInputs.fourButton));
+			}
+			else if (button == "5") {
+				lua_pushnumber(L, static_cast<int>(mouseInputs.fiveButton));
+			}
+			else if (button == "6") {
+				lua_pushnumber(L, static_cast<int>(mouseInputs.sixButton));
+			}
+			else if (button == "7") {
+				lua_pushnumber(L, static_cast<int>(mouseInputs.sevenButton));
+			}
+			else if (button == "8") {
+				lua_pushnumber(L, static_cast<int>(mouseInputs.eightButton));
+			}
 
-		return 1;
+			return 1;
+		}
+		else {
+			NEIGE_SCRIPT_ERROR("Function \"getMouseButtonState(string key)\" takes 1 string parameter.");
+			return 0;
+		}
 	}
 	else {
 		NEIGE_SCRIPT_ERROR("Function \"getMouseButtonState(string key)\" takes 1 string parameter.");
--- src/scripting/VectorScripting.cpp
+++ src/scripting/VectorScripting.cpp
@@ -12,25 +12,37 @@ int VectorScripting::normalize(lua_State* L) {
         float x = static_cast<float>(lua_tonumber(L, 1));
         float y = static_cast<float>(lua_tonumber(L, 2));
 
-        glm::vec2 res = glm::normalize(glm::vec2(x, y));
+        if (lua_isnumber(L, -1) && lua_isnumber(L, -2)) {
+            glm::vec2 res = glm::normalize(glm::vec2(x, y));
 
-        lua_pushnumber(L, res.x);
-        lua_pushnumber(L, res.y);
+            lua_pushnumber(L, res.x);
+            lua_pushnumber(L, res.y);
 
-        return 2;
+            return 2;
+        }
+        else {
+            NEIGE_SCRIPT_ERROR("Function \"normalize(float x, float y, [float z], [float w])\" takes from 2 to 4 float parameters.");
+            return 0;
+        }
     }
     else if (n == 3) {
         float x = static_cast<float>(lua_tonumber(L, 1));
         float y = static_cast<float>(lua_tonumber(L, 2));
         float z = static_cast<float>(lua_tonumber(L, 3));
 
-        glm::vec3 res = glm::normalize(glm::vec3(x, y, z));
+        if (lua_isnumber(L, -1) && lua_isnumber(L, -2) && lua_isnumber(L, -3)) {
+            glm::vec3 res = glm::normalize(glm::vec3(x, y, z));
 
-        lua_pushnumber(L, res.x);
-        lua_pushnumber(L, res.y);
-        lua_pushnumber(L, res.z);
+            lua_pushnumber(L, res.x);
+            lua_pushnumber(L, res.y);
+            lua_pushnumber(L, res.z);
 
-        return 3;
+            return 3;
+        }
+        else {
+            NEIGE_SCRIPT_ERROR("Function \"normalize(float x, float y, [float z], [float w])\" takes from 2 to 4 float parameters.");
+            return 0;
+        }
     }
     else if (n == 4) {
         float x = static_cast<float>(lua_tonumber(L, 1));
@@ -38,14 +50,20 @@ int VectorScripting::normalize(lua_State* L) {
         float z = static_cast<float>(lua_tonumber(L, 3));
         float w = static_cast<float>(lua_tonumber(L, 4));
 
-        glm::vec4 res = glm::normalize(glm::vec4(x, y, z, w));
+        if (lua_isnumber(L, -1) && lua_isnumber(L, -2) && lua_isnumber(L, -3) && lua_isnumber(L, -4)) {
+            glm::vec4 res = glm::normalize(glm::vec4(x, y, z, w));
 
-        lua_pushnumber(L, res.x);
-        lua_pushnumber(L, res.y);
-        lua_pushnumber(L, res.z);
-        lua_pushnumber(L, res.w);
+            lua_pushnumber(L, res.x);
+            lua_pushnumber(L, res.y);
+            lua_pushnumber(L, res.z);
+            lua_pushnumber(L, res.w);
 
-        return 4;
+            return 4;
+        }
+        else {
+            NEIGE_SCRIPT_ERROR("Function \"normalize(float x, float y, [float z], [float w])\" takes from 2 to 4 float parameters.");
+            return 0;
+        }
     }
     else {
         NEIGE_SCRIPT_ERROR("Function \"normalize(float x, float y, [float z], [float w])\" takes from 2 to 4 float parameters.");
@@ -62,14 +80,20 @@ int VectorScripting::dot(lua_State* L) {
         float x2 = static_cast<float>(lua_tonumber(L, 3));
         float y2 = static_cast<float>(lua_tonumber(L, 4));
 
-        glm::vec2 tmp1 = glm::vec2(x1, y1);
-        glm::vec2 tmp2 = glm::vec2(x2, y2);
+        if (lua_isnumber(L, -1) && lua_isnumber(L, -2) && lua_isnumber(L, -3) && lua_isnumber(L, -4)) {
+            glm::vec2 tmp1 = glm::vec2(x1, y1);
+            glm::vec2 tmp2 = glm::vec2(x2, y2);
 
-        float res = glm::dot(tmp1, tmp2);
+            float res = glm::dot(tmp1, tmp2);
 
-        lua_pushnumber(L, res);
+            lua_pushnumber(L, res);
 
-        return 1;
+            return 1;
+        }
+        else {
+            NEIGE_SCRIPT_ERROR("Function \"dot(float x1, float y1, [float z1], [float w1], float x2, float y2, [float z2], [float w2])\" takes 4, 6 or 8 float parameters.");
+            return 0;
+        }
     }
     else if (n == 6) {
         // 2 vec3
@@ -80,14 +104,20 @@ int VectorScripting::dot(lua_State* L) {
         float y2 = static_cast<float>(lua_tonumber(L, 5));
         float z2 = static_cast<float>(lua_tonumber(L, 6));
 
-        glm::vec3 tmp1 = glm::vec3(x1, y1, z1);
-        glm::vec3 tmp2 = glm::vec3(x2, y2, z2);
+        if (lua_isnumber(L, -1) && lua_isnumber(L, -2) && lua_isnumber(L, -3) && lua_isnumber(L, -4) && lua_isnumber(L, -5) && lua_isnumber(L, -6)) {
+            glm::vec3 tmp1 = glm::vec3(x1, y1, z1);
+            glm::vec3 tmp2 = glm::vec3(x2, y2, z2);
 
-        float res = glm::dot(tmp1, tmp2);
+            float res = glm::dot(tmp1, tmp2);
 
-        lua_pushnumber(L, res);
+            lua_pushnumber(L, res);
 
-        return 1;
+            return 1;
+        }
+        else {
+            NEIGE_SCRIPT_ERROR("Function \"dot(float x1, float y1, [float z1], [float w1], float x2, float y2, [float z2], [float w2])\" takes 4, 6 or 8 float parameters.");
+            return 0;
+        }
     }
     else if (n == 8) {
         // 2 vec4
@@ -100,14 +130,20 @@ int VectorScripting::dot(lua_State* L) {
         float z2 = static_cast<float>(lua_tonumber(L, 7));
         float w2 = static_cast<float>(lua_tonumber(L, 8));
 
-        glm::vec4 tmp1 = glm::vec4(x1, y1, z1, w1);
-        glm::vec4 tmp2 = glm::vec4(x2, y2, z2, w2);
+        if (lua_isnumber(L, -1) && lua_isnumber(L, -2) && lua_isnumber(L, -3) && lua_isnumber(L, -4) && lua_isnumber(L, -5) && lua_isnumber(L, -6) && lua_isnumber(L, -7) && lua_isnumber(L, -8)) {
+            glm::vec4 tmp1 = glm::vec4(x1, y1, z1, w1);
+            glm::vec4 tmp2 = glm::vec4(x2, y2, z2, w2);
 
-        float res = glm::dot(tmp1, tmp2);
+            float res = glm::dot(tmp1, tmp2);
 
-        lua_pushnumber(L, res);
+            lua_pushnumber(L, res);
 
-        return 1;
+            return 1;
+        }
+        else {
+            NEIGE_SCRIPT_ERROR("Function \"dot(float x1, float y1, [float z1], [float w1], float x2, float y2, [float z2], [float w2])\" takes 4, 6 or 8 float parameters.");
+            return 0;
+        }
     }
     else {
         NEIGE_SCRIPT_ERROR("Function \"dot(float x1, float y1, [float z1], [float w1], float x2, float y2, [float z2], [float w2])\" takes 4, 6 or 8 float parameters.");
@@ -125,16 +161,22 @@ int VectorScripting::cross(lua_State* L) {
         float y2 = static_cast<float>(lua_tonumber(L, 5));
         float z2 = static_cast<float>(lua_tonumber(L, 6));
 
-        glm::vec3 tmp1 = glm::vec3(x1, y1, z1);
-        glm::vec3 tmp2 = glm::vec3(x2, y2, z2);
+        if (lua_isnumber(L, -1) && lua_isnumber(L, -2) && lua_isnumber(L, -3) && lua_isnumber(L, -4) && lua_isnumber(L, -5) && lua_isnumber(L, -6)) {
+            glm::vec3 tmp1 = glm::vec3(x1, y1, z1);
+            glm::vec3 tmp2 = glm::vec3(x2, y2, z2);
 
-        glm::vec3 res = glm::cross(tmp1, tmp2);
+            glm::vec3 res = glm::cross(tmp1, tmp2);
 
-        lua_pushnumber(L, res.x);
-        lua_pushnumber(L, res.y);
-        lua_pushnumber(L, res.z);
+            lua_pushnumber(L, res.x);
+            lua_pushnumber(L, res.y);
+            lua_pushnumber(L, res.z);
 
-        return 3;
+            return 3;
+        }
+        else {
+            NEIGE_SCRIPT_ERROR("Function \"cross(float x1, float y1, float z1, float x2, float y2, float z2)\" takes 6 float parameters.");
+            return 0;
+        }
     }
     else {
         NEIGE_SCRIPT_ERROR("Function \"cross(float x1, float y1, float z1, float x2, float y2, float z2)\" takes 6 float parameters.");
