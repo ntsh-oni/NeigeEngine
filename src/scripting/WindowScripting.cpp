098529315f72d590004ccc4b7591fb0d96453cc3
ntsh-oni <teamnutshell.dev@gmail.com>
2021-05-20 10:38:00 +0200

    added some window scripting functions

--- src/scripting/WindowScripting.cpp
+++ src/scripting/WindowScripting.cpp
@@ -7,6 +7,8 @@ void WindowScripting::init() {
 	lua_register(L, "setMousePosition", setMousePosition);
 	lua_register(L, "toggleFullscreen", toggleFullscreen);
 	lua_register(L, "getWindowSize", getWindowSize);
+	lua_register(L, "setWindowSize", setWindowSize);
+	lua_register(L, "setWindowTitle", setWindowTitle);
 }
 
 int WindowScripting::isWindowFullscreen(lua_State* L) {
@@ -42,6 +44,36 @@ int WindowScripting::showMouseCursor(lua_State* L) {
 	}
 }
 
+int WindowScripting::getMousePosition(lua_State* L) {
+	int n = lua_gettop(L);
+	if (n == 0) {
+		lua_pushnumber(L, mouseInputs.getXPosition());
+		lua_pushnumber(L, mouseInputs.getYPosition());
+
+		return 2;
+	}
+	else {
+		NEIGE_SCRIPT_ERROR("Function \"getMousePosition()\" takes no parameter.");
+		return 0;
+	}
+}
+
+int WindowScripting::setMousePosition(lua_State* L) {
+	int n = lua_gettop(L);
+	if (n == 2) {
+		double x = lua_tonumber(L, 1);
+		double y = lua_tonumber(L, 2);
+
+		window.setCursorPosition(x, y);
+
+		return 0;
+	}
+	else {
+		NEIGE_SCRIPT_ERROR("Function \"setMousePosition(double x, double y)\" takes 2 parameters.");
+		return 0;
+	}
+}
+
 int WindowScripting::toggleFullscreen(lua_State* L) {
 	int n = lua_gettop(L);
 	if (n == 0) {
@@ -69,32 +101,45 @@ int WindowScripting::getWindowSize(lua_State* L) {
 	}
 }
 
-int WindowScripting::getMousePosition(lua_State* L) {
+int WindowScripting::setWindowSize(lua_State* L) {
 	int n = lua_gettop(L);
-	if (n == 0) {
-		lua_pushnumber(L, mouseInputs.getXPosition());
-		lua_pushnumber(L, mouseInputs.getYPosition());
+	if (n == 2) {
+		if (lua_isnumber(L, -1) && lua_isnumber(L, -2)) {
+			int width = static_cast<int>(lua_tonumber(L, 1));
+			int height = static_cast<int>(lua_tonumber(L, 2));
 
-		return 2;
+			window.setWindowSize(width, height);
+
+			return 0;
+		}
+		else {
+			NEIGE_SCRIPT_ERROR("Function \"setWindowSize(int width, int height)\" takes 2 integer parameters.");
+			return 0;
+		}
 	}
 	else {
-		NEIGE_SCRIPT_ERROR("Function \"getMousePosition()\" takes no parameter.");
+		NEIGE_SCRIPT_ERROR("Function \"setWindowSize(int width, int height)\" takes 2 integer parameters.");
 		return 0;
 	}
 }
 
-int WindowScripting::setMousePosition(lua_State* L) {
+int WindowScripting::setWindowTitle(lua_State* L) {
 	int n = lua_gettop(L);
-	if (n == 2) {
-		double x = lua_tonumber(L, 1);
-		double y = lua_tonumber(L, 2);
+	if (n == 1) {
+		if (lua_isstring(L, -1)) {
+			std::string title = lua_tostring(L, 1);
 
-		window.setCursorPosition(x, y);
+			window.setWindowTitle(title);
 
-		return 0;
+			return 0;
+		}
+		else {
+			NEIGE_SCRIPT_ERROR("Function \"setWindowTitle(string title)\" takes 1 string parameter.");
+			return 0;
+		}
 	}
 	else {
-		NEIGE_SCRIPT_ERROR("Function \"setMousePosition(double x, double y)\" takes 2 parameters.");
+		NEIGE_SCRIPT_ERROR("Function \"setWindowTitle(string title)\" takes 1 string parameter.");
 		return 0;
 	}
 }
